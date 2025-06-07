5b520f512c32e4aa88868c1d74b86e1997d586c8
Jan Gabrielsson <jan@gabrielsson.com>
2025-06-07 06:27:55 +0200

    feat: Add QA manager and selectable classes for improved QuickApp management

---
+++ examples/QA_manager.lua
@@ -0,0 +1,44 @@
+--%%name=Manager
+--%%type=com.fibaro.deviceController
+--%%webui=true
+
+--%%file=$hc3emu2.lib.selectable,selectable
+--%%u={label='l1',text='QuickApp Manager'}
+--%%u={select='qaSelect', text="QuickApp", onToggled="qaSelect"}
+--%%u={select='varSelect', text="QuickApp", onToggled="varSelect"}
+
+---@class QAList
+QAList = {}
+class "QAList"(Selectable)
+function QAList:__init(qa) Selectable.__init(self,qa,"qaSelect") end
+function QAList:text(item) return item.id..":"..item.name end
+function QAList:value(item) return item.id end
+function QAList:sort(a,b) 
+  return a.name < b.name
+end
+function QAList:selected(item)
+  local vars = item.properties.quickAppVariables or {}
+  self.qa.variables:update(vars)
+  self.qa:updateView("value","text","")
+end
+
+---@class QAVars
+QAVars = {}
+class "QAVars"(Selectable)
+function QAVars:__init(qa) Selectable.__init(self,qa,"varSelect") end
+function QAVars:text(item) return item.name end
+function QAVars:value(item) return item.name end
+function QAVars:sort(a,b) return a.name < b.name end
+function QAVars:selected(item)
+  self.qa:updateView("value","text",tostring(item.value))
+end
+
+function QuickApp:onInit()
+    self:debug("onInit")
+    self:updateView("l1","text","QuickApp Manager2")
+    self.list = QAList(self)
+    self.variables = QAVars(self)
+    local qas = api.get("/devices?interface=quickApp")
+    self.list:update(qas)
+end
+§
