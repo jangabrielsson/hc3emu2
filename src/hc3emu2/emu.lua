788ac5d1270965d110f929c30849d54f3c34058c
Jan Gabrielsson <jan@gabrielsson.com>
2025-06-21 13:06:37 +0200

    Update rockspecs/hc3emu2-2.0.27-1.rockspec to version 2.0.27

--- src/hc3emu2/emu.lua
+++ src/hc3emu2/emu.lua
@@ -39,7 +39,7 @@
 ---@see hc3emu2.api - API handling
 ---@see hc3emu2.config - Configuration management
 
-local VERSION = "2.0.26"
+local VERSION = "2.0.27"
 local mode, mainFile, runflags, taskArgs = ...
 
 local fmt = string.format
